1e8b280088c3c6ac6166f48a5d5df9f2cd0bdf29
monipmark <90512420+monipmark@users.noreply.github.com>
2023-07-13 00:15:26 +0700

    first commit

---
+++ Clear Cache  (Run-As-Admin).bat
@@ -0,0 +1,29 @@
+@echo off
+title Cop's Tweaks Pack /\ Clear Cache
+cd/
+@echo
+del *.log /a /s /q /f
+del /s /f /q c:\windows\temp\*.*
+del /s /f /q C:\WINDOWS\Prefetch
+del /s /f /q %temp%\*.*
+deltree /y c:\windows\tempor~1
+deltree /y c:\windows\temp
+deltree /y c:\windows\tmp
+deltree /y c:\windows\ff*.tmp
+deltree /y c:\windows\history
+deltree /y c:\windows\cookies
+deltree /y c:\windows\recent
+deltree /y c:\windows\spool\printers
+net stop wuauserv
+net stop UsoSvc
+rd /s /q C:\Windows\SoftwareDistribution
+md C:\Windows\SoftwareDistribution
+RD /S /Q %temp%
+MKDIR %temp%
+takeown /f "%temp%" /r /d y
+takeown /f "C:\Windows\Temp" /r /d y
+RD /S /Q C:\Windows\Temp
+MKDIR C:\Windows\Temp
+takeown /f "C:\Windows\Temp" /r /d y
+takeown /f %temp% /r /d y
+cls
---
+++ autodel.bat
@@ -0,0 +1,3 @@
+rmdir /s /q %temp%
+
+mb %temp%
